dbdd936d03532fd15f5b53dfdc79d8e2f0222d13
geisondossantos <geison_bf@hotmail.com>
2022-06-16 12:44:34 -0300

    Ajusta base para o projeto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Login from '../pages/Login';
-import Home from '../pages/Home';
-import Page404 from '../pages/Page404';
 import MyRoute from './MyRoute';
+
+import Aluno from '../pages/Aluno';
+import Alunos from '../pages/Alunos';
+import Fotos from '../pages/Fotos';
+import Register from '../pages/Register';
+import Login from '../pages/Login';
+import Page404 from '../pages/Page404';
 
 export default function Routers() {
   return (
     <Routes>
       <Route exact path="/" element={<MyRoute />}>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/aluno/:id/edit" element={<Aluno />} />
+        <Route exact path="/aluno/" element={<Aluno />} />
+        <Route exact path="/fotos/:id" element={<Fotos />} />
       </Route>
 
-      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/alunos" element={<Alunos />} />
+      <Route exact path="/register/" element={<Register />} />
+      <Route exact path="/login/" element={<Login />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   );

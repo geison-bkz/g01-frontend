45ef2fcd577adefb39d3d69e5288993c7e2f6283
geisondossantos <geison_bf@hotmail.com>
2022-06-20 18:18:39 -0300

    Adiciona pagina de registro

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -8,17 +8,14 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case types.BOTAO_CLICADO_SUCCESS: {
-      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
     case types.BOTAO_CLICADO_FAILURE: {
-      console.log('Deu erro');
       return state;
     }
     case types.BOTAO_CLICADO_REQUEST: {
-      console.log('Esto fazendo a requisição');
       return state;
     }
 
